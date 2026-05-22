493ea9e3d8751e8f1d73ec56a33f508478e9bf49
lotress <cmyan6@gmail.com>
2026-05-22 15:52:30 +0800

    pack commands

---
+++ pack-exe.bat
@@ -0,0 +1,1 @@
+c:\soft\7-Zip\7z.exe a standalone-all-in-one.zip UEDataMerge.exe config.json tools/ output/ patches/ "-x!tools/oo2core_9_win64.dll" "-x!output/*" "-x!patches/*"
---
+++ pack-plugin.bat
@@ -0,0 +1,1 @@
+c:\soft\7-Zip\7z.exe a plugin-all-in-one.zip *.py config.json .gitignore build.bat tools/ output/ patches/ "-x!tools/oo2core_9_win64.dll" "-x!output/*" "-x!patches/*"
