b71c3ef5ab5273cfabcaae713234c33c9cdac801
lotress <cmyan6@gmail.com>
2026-05-26 18:10:40 +0800

    update progress bar

--- __init__.py
+++ __init__.py
@@ -254,14 +254,15 @@ class Plugin(mobase.IPluginTool):
       assetsToPatch = tools.mixinUserMods(assetsToPatch, self.__args.all)
       packages = tools.filterPackages(packages, assetsToPatch)
       dialog.setTitle(f'Merging {len(assetsToPatch)} data tables of {count} mod packages.')
+      dialog.setRange(0, total)
+      progress = 0
+      dialog.setValue(progress)
       tools.prepare(packages)
       dialog.setStatus(self.tr('Unpacking data tables...'))
       for package in packages:
         tools.unpack(package, package)
       tools.unpackBase(assetsToPatch)
       total = sum(map(len, assetsToPatch.values()))
-      dialog.setRange(0, total)
-      progress = 0
       for asset, mods in assetsToPatch.items():
         dialog.setStatus(f'Processing asset: {asset}')
         for package in tools.mergeAsset(asset, mods):
